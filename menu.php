ad39d5448f4e56e4c184c65227d280411989fc8e
PIGSx <hiury1315ajac@gmail.com>
2023-02-09 17:08:44 -0300

    criando o primeiro molde do menu

---
+++ menu.php
@@ -0,0 +1,39 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/menu.css">
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+    <nav>
+    <div class="container">
+        <input type="checkbox" name="" id="check">
+        <a href="#!">
+            <h3>ADVOCACY</h3>
+        </a>
+        <ul class="links">
+            <li><a class="active" href="#!">home</a></li>
+            <li><a href="#!">about</a></li>
+            <li><a href="#!">services</a></li>
+            <li><a href="#!">contact</a></li>
+        </ul>
+        <div class="hamburguer-menu-container">
+            <div class="hamburguer-menu">
+                <div></div>
+            </div>
+        </div>
+    </div>
+</nav>
+<script src="js/menu.js"></script>
+</body>
+
+</html>
