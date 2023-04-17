f683c691197387f4234fb76d4f16f78686bd6d53
PIGSx <hiury1315ajac@gmail.com>
2023-04-17 18:41:31 -0300

    aumentando pagina

--- adv.php
+++ adv.php
@@ -4,62 +4,126 @@ include('menu.php');
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/adv.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/adv.css">
+  <link rel="stylesheet" href="css/swiper-bundle.min.css" />
 </head>
 
 <body class="dark">
   <section class="about">
-      <div class="header-body">
-        <div class="banner">
-          <div class="decoration-left animation"></div>
-          <div class="decoration-rigth animation"></div>
-          <div class="overlay animation"></div>
-          <img src="img/advogados/adv.jpg" alt="" class="banner-img">
+    <div class="header-body">
+      <div class="banner">
+        <div class="decoration-left animation"></div>
+        <div class="decoration-rigth animation"></div>
+        <div class="overlay animation"></div>
+        <img src="img/advogados/adv.jpg" alt="" class="banner-img">
+      </div>
+      <div class="text">
+        <div>
+          <h1>PIGS LINDO</h1>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book.</p>
         </div>
-        <div class="text">
-          <div>
-            <h1>PIGS LINDO</h1>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+      </div>
+    </div>
+  </section>
+  <section class="timeline">
+    <div class="main">
+      <div class="container">
+        <ul>
+          <li>
+            <h3 class="title">PIGS</h3>
+            <p>vou te buscar e contar meus planos, acho que cê vai gostar</p>
+            <span class="circle"></span>
+            <span class="date">educação</span>
+          </li>
+          <li>
+            <h3 class="title">PIGS</h3>
+            <p>vou te buscar e contar meus planos, acho que cê vai gostar</p>
+            <span class="circle"></span>
+            <span class="date">experiência</span>
+          </li>
+          <li>
+            <h3 class="title">PIGS</h3>
+            <p>vou te buscar e contar meus planos, acho que cê vai gostar</p>
+            <span class="circle"></span>
+            <span class="date">educação</span>
+          </li>
+          <li>
+            <h3 class="title">PIGS</h3>
+            <p>vou te buscar e contar meus planos, acho que cê vai gostar</p>
+            <span class="circle"></span>
+            <span class="date">experiência</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  <header>
+      <h1>Atuações</h1>
+    </header>
+  <section class="slide">
+  <div class="container swiper">
+      <div class="slide-container">
+        <div class="card-wrapper swiper-wrapper">
+          <div class="card swiper-slide">
+            <div class="image-box">
+              <img src="img/advogados/atu1.jpg" alt="" />
+            </div>
+            <div class="profile-details">
+              <div class="name-job">
+                <h3 class="name">nome</h3>
+                <h4 class="area">atuacao</h4>
+              </div>
+            </div>
+          </div>
+          <div class="card swiper-slide">
+            <div class="image-box">
+            <img src="img/advogados/atu2.jpg" alt="" />
+            </div>
+            <div class="profile-details">
+              <div class="name-job">
+              <h3 class="name">nome</h3>
+                <h4 class="area">atuacao</h4>
+              </div>
+            </div>
+          </div>
+          <div class="card swiper-slide">
+            <div class="image-box">
+            <img src="img/advogados/atu3.jpg" alt="" />
+            </div>
+            <div class="profile-details">
+              <div class="name-job">
+              <h3 class="name">nome</h3>
+                <h4 class="area">atuacao</h4>
+              </div>
+            </div>
+          </div>
+          <div class="card swiper-slide">
+            <div class="image-box">
+            <img src="img/advogados/yv.jpg" alt="" />
+            </div>
+            <div class="profile-details">
+              <div class="name-job">
+              <h3 class="name">nome</h3>
+                <h4 class="area">atuacao</h4>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </section>
-    <section class="timeline">
-      <div class="main">
-        <div class="container">
-          <ul>
-            <li>
-              <h3 class="title">PIGS</h3>
-              <p>vou te buscar e contar meus planos, acho que cê vai gostar</p>
-              <span class="circle"></span>
-              <span class="date">educação</span>
-            </li>
-            <li>
-              <h3 class="title">PIGS</h3>
-              <p>vou te buscar e contar meus planos, acho que cê vai gostar</p>
-              <span class="circle"></span>
-              <span class="date">experiência</span>
-            </li>
-            <li>
-              <h3 class="title">PIGS</h3>
-              <p>vou te buscar e contar meus planos, acho que cê vai gostar</p>
-              <span class="circle"></span>
-              <span class="date">educação</span>
-            </li>
-            <li>
-              <h3 class="title">PIGS</h3>
-              <p>vou te buscar e contar meus planos, acho que cê vai gostar</p>
-              <span class="circle"></span>
-              <span class="date">experiência</span>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </section>
-<script src="js/dark.js"></script>
+      <div class="swiper-button-next swiper-navBtn"></div>
+      <div class="swiper-button-prev swiper-navBtn"></div>
+      <div class="swiper-pagination"></div>
+    </div>
+  </section>
+  <script src="js/dark.js"></script>
+  <script src="js/swiper-bundle.min.js"></script>
+  <script src="js/adv.js"></script>
 </body>
+
 </html>
