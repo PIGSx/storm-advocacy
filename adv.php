18e3e4431e003bd74c7cc35763654031786b5f1f
PIGSx <hiury1315ajac@gmail.com>
2023-04-16 22:13:57 -0300

    criando nova pagina de advogado

---
+++ adv.php
@@ -0,0 +1,65 @@
+<?php
+include('menu.php');
+?>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/adv.css">
+</head>
+
+<body class="dark">
+  <section class="about">
+      <div class="header-body">
+        <div class="banner">
+          <div class="decoration-left animation"></div>
+          <div class="decoration-rigth animation"></div>
+          <div class="overlay animation"></div>
+          <img src="img/advogados/adv.jpg" alt="" class="banner-img">
+        </div>
+        <div class="text">
+          <div>
+            <h1>PIGS LINDO</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="timeline">
+      <div class="main">
+        <div class="container">
+          <ul>
+            <li>
+              <h3 class="title">PIGS</h3>
+              <p>vou te buscar e contar meus planos, acho que cê vai gostar</p>
+              <span class="circle"></span>
+              <span class="date">educação</span>
+            </li>
+            <li>
+              <h3 class="title">PIGS</h3>
+              <p>vou te buscar e contar meus planos, acho que cê vai gostar</p>
+              <span class="circle"></span>
+              <span class="date">experiência</span>
+            </li>
+            <li>
+              <h3 class="title">PIGS</h3>
+              <p>vou te buscar e contar meus planos, acho que cê vai gostar</p>
+              <span class="circle"></span>
+              <span class="date">educação</span>
+            </li>
+            <li>
+              <h3 class="title">PIGS</h3>
+              <p>vou te buscar e contar meus planos, acho que cê vai gostar</p>
+              <span class="circle"></span>
+              <span class="date">experiência</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+<script src="js/dark.js"></script>
+</body>
+</html>
