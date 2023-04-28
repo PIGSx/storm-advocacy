47ea619befd4e4e64c8dc6fe83ca013ded8e924f
PIGSx <hiury1315ajac@gmail.com>
2023-04-28 11:28:58 -0300

    criando pagina de error

---
+++ error.php
@@ -0,0 +1,143 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>oh, sorry..</title>
+    <link rel="stylesheet" href="css/error.css">
+</head>
+
+<body>
+    <div class="container container-star">
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-1"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+        <div class="star-2"></div>
+    </div>
+    <div class="container container-bird">
+        <div class="bird bird-anim">
+            <div class="bird-container">
+                <div class="wing wing-left">
+                    <div class="wing-left-top"></div>
+                </div>
+                <div class="wing wing-right">
+                    <div class="wing-right-top"></div>
+                </div>
+            </div>
+        </div>
+        <div class="bird bird-anim">
+            <div class="bird-container">
+                <div class="wing wing-left">
+                    <div class="wing-left-top"></div>
+                </div>
+                <div class="wing wing-right">
+                    <div class="wing-right-top"></div>
+                </div>
+            </div>
+        </div>
+        <div class="bird bird-anim">
+            <div class="bird-container">
+                <div class="wing wing-left">
+                    <div class="wing-left-top"></div>
+                </div>
+                <div class="wing wing-right">
+                    <div class="wing-right-top"></div>
+                </div>
+            </div>
+        </div>
+        <div class="bird bird-anim">
+            <div class="bird-container">
+                <div class="wing wing-left">
+                    <div class="wing-left-top"></div>
+                </div>
+                <div class="wing wing-right">
+                    <div class="wing-right-top"></div>
+                </div>
+            </div>
+        </div>
+        <div class="bird bird-anim">
+            <div class="bird-container">
+                <div class="wing wing-left">
+                    <div class="wing-left-top"></div>
+                </div>
+                <div class="wing wing-right">
+                    <div class="wing-right-top"></div>
+                </div>
+            </div>
+        </div>
+        <div class="bird bird-anim">
+            <div class="bird-container">
+                <div class="wing wing-left">
+                    <div class="wing-left-top"></div>
+                </div>
+                <div class="wing wing-right">
+                    <div class="wing-right-top"></div>
+                </div>
+            </div>
+        </div>
+        <div class="txt">
+            <h1>404</h1>
+            <h3>Oops. page not found :´(</h3>
+            <a href="index.php" class="btn">Go back</a>
+        </div>
+    </div>
+</body>
+
+</html>
