9d5b648e1a322002a7d02020490661a4b473deea
PIGSx <hiury1315ajac@gmail.com>
2023-05-04 15:42:19 -0300

    criando o menu na versao com e sem login

--- menu.php
+++ menu.php
@@ -15,29 +15,70 @@
 </head>
 
 <body>
-    <div class="menu">
-    <nav>
-    <div class="container">
-        <input type="checkbox" name="" id="check">
-        <a href="index.php">
-            <h3>ADVOCACY</h3>
-        </a>
-        <ul class="links">
-            <li><a class="active" href="index.php">início</a></li>
-            <li><a href="about.php">sobre</a></li>
-            <li><a href="atuacoes.php">áreas</a></li>
-            <li><a href="#!">contato</a></li>
-        </ul>
-        <i class="uil uil-moon toggle-btn"></i>
-        <div class="hamburguer-menu-container">
-            <div class="hamburguer-menu">
-                <div></div>
-            </div>
-        </div>
-    </div>
-</nav>
-</div>
-<script src="js/menu.js"></script>
+    <?php
+    @session_start();
+    // @var_dump($_SESSION);
+    if (!isset($_SESSION['login'])) {
+        $_SESSION['login'] = false;
+    }
+    if ($_SESSION['login'] === true) {
+        echo "<div class=menu>";
+        echo "<nav>";
+        echo " <div class=container>";
+        echo  "<input type=checkbox id=check>";
+        echo  "<a href=index.php>";
+        echo      "<h3>ADVOCACY</h3>";
+        echo  " </a>";
+        echo   "<ul class=links>";
+        echo      "<li><a class=active href=index.php>início</a></li>";
+        echo      "<li><a href=about.php>sobre</a></li>";
+        echo      "<li><a href=atuacoes.php>áreas</a></li>";
+        echo      "<li><a href=contato.php>contato</a></li>";
+        echo  "</ul>";
+        echo     "<ul class=icons>";
+        echo  "<div class=icons>";
+        echo  '<i class="uil uil-moon toggle-btn"></i>';
+        echo   '<a href=perfil.php><i class="uil uil-user"></i></a>';
+        echo "</div>";
+        echo "</ul>";
+        echo  "<div class=hamburguer-menu-container>";
+        echo     "  <div class=hamburguer-menu>";
+        echo            "<div></div>";
+        echo        "</div>";
+        echo   " </div>";
+        echo "</div>";
+        echo "</nav>";
+        echo "</div>";
+    } else {
+        echo "<div class=menu>";
+        echo "<nav>";
+        echo " <div class=container>";
+        echo  "<input type=checkbox id=check>";
+        echo  "<a href=index.php>";
+        echo      "<h3>ADVOCACY</h3>";
+        echo  " </a>";
+        echo   "<ul class=links>";
+        echo      "<li><a class=active href=index.php>início</a></li>";
+        echo      "<li><a href=about.php>sobre</a></li>";
+        echo      "<li><a href=atuacoes.php>áreas</a></li>";
+        echo      "<li><a href=#!>contato</a></li>";
+        echo  "</ul>";
+        echo     "<ul class=icons>";
+        echo  "<div class=icons>";
+        echo  '<i class="uil uil-moon toggle-btn"></i>';
+        echo "</div>";
+        echo "</ul>";
+        echo  "<div class=hamburguer-menu-container>";
+        echo     "  <div class=hamburguer-menu>";
+        echo            "<div></div>";
+        echo        "</div>";
+        echo   " </div>";
+        echo "</div>";
+        echo "</nav>";
+        echo "</div>";
+    }
+    ?>
+    <script src="js/menu.js"></script>
 </body>
 
 </html>
