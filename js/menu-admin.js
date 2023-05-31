f09b97cac5a340e0710e37c50916d69bb0cace2b
PIGSx <hiury1315ajac@gmail.com>
2023-05-31 01:28:13 -0300

    organizando o active do menu

--- js/menu-admin.js
+++ js/menu-admin.js
@@ -1,18 +1,23 @@
 const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
 
-allSideMenu.forEach(item=> {
-	const li = item.parentElement;
+allSideMenu.forEach(item => {
+  const li = item.parentElement;
 
-	item.addEventListener('click', function () {
-		allSideMenu.forEach(i=> {
-			i.parentElement.classList.remove('active');
-		})
-		li.classList.add('active');
-	})
+  item.addEventListener('click', function() {
+    allSideMenu.forEach(i => {
+      i.parentElement.classList.remove('active');
+    });
+    li.classList.add('active');
+  });
+
+  // comparando a URL
+  if (item.href === window.location.href) {
+    allSideMenu.forEach(i => {
+      i.parentElement.classList.remove('active');
+    });
+    li.classList.add('active');
+  }
 });
-
-
-
 
 // close menu
 const menuBar = document.querySelector('#content nav .bx.bx-menu');
