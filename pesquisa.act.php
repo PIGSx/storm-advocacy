46ccd04fe9f8ddb1c7d54d351c0c584bfaf388f9
PIGSx <hiury1315ajac@gmail.com>
2023-06-01 00:45:23 -0300

    criando um leve estilo para a pesquisa

--- pesquisa.act.php
+++ pesquisa.act.php
@@ -5,44 +5,49 @@
     $texto = $_GET['texto'];
 
     require('connect.php');
-        if(!$texto == ""){
-            $perfis = mysqli_query($con, "SELECT * FROM `cadastro_login_cliente`  where `nome` like '%$texto%'");
-            echo       "<table width=100%>";
-            echo              "<thead>";
-            echo                "<tr>";
-            echo                     "<th>#</th>";
-            echo                    "<th></span>CLIENTE</th>";
-            echo                    "<th></span>CPF</th>";
-            echo                  "<th></span>STATUS</th>";
-            echo                  "<th></span>DADOS</th>";
+    if(!$texto == ""){
+        $perfis = mysqli_query($con, "SELECT * FROM `cadastro_login_cliente`  where `nome` like '%$texto%'");
+        
+        if (mysqli_num_rows($perfis) == 0) {
+            echo "<p id=no-file>nenhum arquivo encontrado</p>";
+        } else {
+            echo "<table width=100%>";
+            echo "<thead>";
+            echo "<tr>";
+            echo "<th>#</th>";
+            echo "<th></span>CLIENTE</th>";
+            echo "<th></span>CPF</th>";
+            echo "<th></span>STATUS</th>";
+            echo "<th></span>DADOS</th>";
+            echo "</tr>";
+            echo "</thead>";
+            echo "<tbody>";
             while ($perfil = mysqli_fetch_array($perfis)) {
-                echo             "</tr>";
-                echo        "</thead>";
-                echo        "<tbody>";
-                echo          "<tr>";
-                echo              "<td class=cod>$perfil[cod]</td>";
-                echo             "<td>";
-                echo   "<div class=client>";
-                echo   "<div class=client-info>";
-                echo  "<h4>$perfil[nome]</h4>";
-                echo  "<small>$perfil[email]</small>";
-                echo   "</div>";
-                echo  "</div>";
-                echo  "</td>";
-                echo  "<td>$perfil[cpf]</td>";
-                echo  "<td>";
-                echo  "<span class=status>$perfil[status]</span>";
+                echo "<tr>";
+                echo "<td class=cod>$perfil[cod]</td>";
+                echo "<td>";
+                echo "<div class=client>";
+                echo "<div class=client-info>";
+                echo "<h4>$perfil[nome]</h4>";
+                echo "<small>$perfil[email]</small>";
+                echo "</div>";
+                echo "</div>";
                 echo "</td>";
-                echo  "<td>";
-                echo  "<div id=icones>";
-                echo  "<a href=./formularios/alterar-status.php?cod=$perfil[cod]><i class='uil uil-edit'></i></a>";
-                echo  "<a href=javascript:confirmar($perfil[cod])><i class='uil uil-trash-alt'></i></a>";
-                echo  "</div>";
+                echo "<td>$perfil[cpf]</td>";
+                echo "<td>";
+                echo "<span class=status>$perfil[status]</span>";
                 echo "</td>";
+                echo "<td>";
+                echo "<div id=icones>";
+                echo "<a href=./formularios/alterar-status.php?cod=$perfil[cod]><i class='uil uil-edit'></i></a>";
+                echo "<a href=javascript:confirmar($perfil[cod])><i class='uil uil-trash-alt'></i></a>";
+                echo "</div>";
+                echo "</td>";
+                echo "</tr>";
             }
-            echo  "</tr>";
-            echo        "</tbody>";
-            echo   "</table>";
-            echo   "</div>";
+            echo "</tbody>";
+            echo "</table>";
+            echo "</div>";
         }
+    }
 ?>
--- pesquisaJ.act.php
+++ pesquisaJ.act.php
@@ -1,48 +1,52 @@
 <link rel="stylesheet" href="css/admin.css">
 <?php
-    @session_start();
+@session_start();
 
-    $texto = $_GET['texto'];
+$texto = $_GET['texto'];
 
-    require('connect.php');
-        if(!$texto == ""){
-            $perfis = mysqli_query($con, "SELECT * FROM tb_cliente_juridico`  where `nome` like '%$texto%'");
-            echo       "<table width=100%>";
-            echo              "<thead>";
-            echo                "<tr>";
-            echo                     "<th>#</th>";
-            echo                    "<th></span>CLIENTE</th>";
-            echo                    "<th></span>CPF</th>";
-            echo                  "<th></span>STATUS</th>";
-            echo                  "<th></span>DADOS</th>";
-            while ($perfil = mysqli_fetch_array($perfis)) {
-                echo             "</tr>";
-                echo        "</thead>";
-                echo        "<tbody>";
-                echo          "<tr>";
-                echo              "<td class=cod>$perfil[cod]</td>";
-                echo             "<td>";
-                echo   "<div class=client>";
-                echo   "<div class=client-info>";
-                echo  "<h4>$perfil[nome]</h4>";
-                echo  "<small>$perfil[email]</small>";
-                echo   "</div>";
-                echo  "</div>";
-                echo  "</td>";
-                echo  "<td>$perfil[cnpj]</td>";
-                echo  "<td>";
-                echo  "<span class=status>$perfil[status]</span>";
-                echo "</td>";
-                echo  "<td>";
-                echo  "<div id=icones>";
-                echo  "<a href=./formularios/alterar-status.php?cod=$perfil[cod]><i class='uil uil-edit'></i></a>";
-                echo  "<a href=javascript:confirmar($perfil[cod])><i class='uil uil-trash-alt'></i></a>";
-                echo  "</div>";
-                echo "</td>";
-            }
-            echo  "</tr>";
-            echo        "</tbody>";
-            echo   "</table>";
+require('connect.php');
+if (!$texto == "") {
+    $perfis = mysqli_query($con, "SELECT * FROM `tb_cliente_juridico` where `nome` like '%$texto%'");
+    if (mysqli_num_rows($perfis) == 0) {
+        echo "<p id=no-file>nenhum arquivo encontrado</p>";
+    } else {
+        echo       "<table width=100%>";
+        echo              "<thead>";
+        echo                "<tr>";
+        echo                     "<th>#</th>";
+        echo                    "<th></span>CLIENTE</th>";
+        echo                    "<th></span>CPF</th>";
+        echo                  "<th></span>STATUS</th>";
+        echo                  "<th></span>DADOS</th>";
+        while ($perfil = mysqli_fetch_array($perfis)) {
+            echo             "</tr>";
+            echo        "</thead>";
+            echo        "<tbody>";
+            echo          "<tr>";
+            echo              "<td class=cod>$perfil[cod]</td>";
+            echo             "<td>";
+            echo   "<div class=client>";
+            echo   "<div class=client-info>";
+            echo  "<h4>$perfil[nome]</h4>";
+            echo  "<small>$perfil[email]</small>";
             echo   "</div>";
+            echo  "</div>";
+            echo  "</td>";
+            echo  "<td>$perfil[cnpj]</td>";
+            echo  "<td>";
+            echo  "<span class=status>ativo</span>";
+            echo "</td>";
+            echo  "<td>";
+            echo  "<div id=icones>";
+            echo  "<a href=./formularios/alterar-adv.php?cod=$perfil[cod]><i class='uil uil-edit'></i></a>";
+            echo  "<a href=javascript:confirmar($perfil[cod])><i class='uil uil-trash-alt'></i></a>";
+            echo  "</div>";
+            echo "</td>";
         }
+        echo  "</tr>";
+        echo        "</tbody>";
+        echo   "</table>";
+        echo   "</div>";
+    }
+}
 ?>
